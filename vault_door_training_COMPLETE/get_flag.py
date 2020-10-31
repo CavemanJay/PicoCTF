38e6daf213793f7330fa82827b516d69e5b3edea
JayCuevas <Jordan.Cuevas@precisecal.com>
2020-10-30 23:05:45 -0400

    Updated vault door training challenge

--- vault_door_training_COMPLETE/get_flag.py
+++ vault_door_training_COMPLETE/get_flag.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import os
 
-flag = os.popen(
-    "curl -s https://2019shell1.picoctf.com/static/0a9478bb281651d883f8148672560fa1/VaultDoorTraining.java | tail -n 3 | head -n 1").read()
-flag = flag[flag.find('"')+1:].split('"')[0]
-flag = F"picoCTF{{{flag}}}"
-print(flag)
+with open('./VaultDoorTraining.java') as source_file:
+    src = source_file.readlines()[-3]
+    flag = src[src.find('"')+1:].split('"')[0]
+    flag = F"picoCTF{{{flag}}}"
+    print(flag)
